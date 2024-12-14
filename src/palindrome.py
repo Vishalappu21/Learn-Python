2a22d78ad6509a4ae4156c15a66dd4c4535d5778
Vishal Prasanna <vishalanbu582000@gmail.com>
2024-12-14 23:06:05 +0530

    New code

--- src/palindrome.py
+++ src/palindrome.py
@@ -1,12 +1,12 @@
 def ispalindrome(s):
-   return s == s[::-1]
-s = "Vishal"
-A = ispalindrome(s)
-if A:
-   print("Yes")
-else:
-   print("No")
-   
-for A in range(0,100):
-   if all(A % i !=0 for i in range(2,A)):
-     print(A, "Prime Number")
+-   return s == s[::-1]
+-s = "Vishal"
+-A = ispalindrome(s)
+-if A:
+-   print("Yes")
+-else:
+-   print("No")
+-   
+-for A in range(0,100):
+-   if all(A % i !=0 for i in range(2,A)):
+-     print(A, "Prime Number")
