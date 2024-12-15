ffd219e268a5eb1b81e46e87074f9af3c465a246
Vishal Prasanna <vishalanbu582000@gmail.com>
2024-12-15 17:12:08 +0530

    Logic for finding the maximum Number

---
+++ src/FindingMaximunnumber.py
@@ -0,0 +1,7 @@
+Num1 = int(input("Enter the Nuumber1:"))
+Num2 = int(input("Enter the Nuumber2:"))
+
+if Num1>Num2:
+    print("Num1 is Maximun than the Num2")
+else:
+    print("Num2 is Minimum than the Num1")
