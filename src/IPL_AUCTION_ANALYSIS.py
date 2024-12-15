682c41965238d8274844a69aedb54bde717f3222
Vishal Prasanna <vishalanbu582000@gmail.com>
2024-12-15 14:07:17 +0530

    Updated and correction in Prime_number logic

---
+++ src/IPL_AUCTION_ANALYSIS.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+IPL_df = pd.read_csv("C:/Users/vishalappu/Downloads/ipl_2025_auction_players.csv")
+IPL_df
--- src/Prime.py
+++ src/Prime.py
@@ -1,11 +1,26 @@
-num_int = int(input("Enter_the_number: "))
+num_int = int(input("Enter the number: "))
+
 if num_int == 1:
-    print("is not a prime_number")
-if num_int>1:
-    for n in range(2,num_int):
-        if num_int%2 == 0:
-            print(num_int,"is not a prime_number")
+    print(f"{num_int} is not a prime number.")
+    print("Thank you")
+elif num_int > 1:
+    for n in range(2, num_int):
+        if num_int % n == 0:
+            print(f"{num_int} is not a prime number.")
             break
-        else:
-            print(num_int,"is a prime_number")
-        
+    else:
+        print(f"{num_int} is a prime number.")
+
+    
+    print(f"Prime numbers between 0 and {num_int}:")
+    for i in range(2, num_int + 1):
+        is_prime = True
+        for j in range(2, int(i**0.5) + 1):
+            if i % j == 0:
+                is_prime = False
+                break
+        if is_prime:
+            print(i, end=" ")
+
+if num_int > 1 and not all(num_int % n != 0 for n in range(2, num_int)):
+    print("\nThank you")
