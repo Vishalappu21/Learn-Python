f8ca9b67b6459da0819eb4905d7b7e004bfa35c4
Vishal Prasanna <vishalanbu582000@gmail.com>
2024-12-15 20:28:38 +0530

    Logic correction in Prime number

--- src/Prime.py
+++ src/Prime.py
@@ -1,5 +1,5 @@
 def is_prime(num):
-    if num < 2:
+    if num < 1:
         return False
     for i in range(2, int(num**0.5) + 1):
         if num % i == 0:
@@ -8,28 +8,28 @@ def is_prime(num):
 
 def primes_in_range(start, end):
     primes = []
-    for num in range(start, end + 1):
-        if is_prime(num):
-            primes.append(num)
+    for ind in range(start, end + 1):
+        if is_prime(ind):
+            primes.append(ind)
     return primes
 
 # Input from the user
 while True:
     try:
-        num = int(input("Enter the number: "))
+        number = int(input("Enter the number: "))
         break
     except ValueError:
         print("Entered value is not a valied input, expecting a whole integer number. Please enter again!")
-if is_prime(num):
-    print(f"{num} is a prime number")
+if is_prime(number):
+    print(f"{number} is a prime number")
 else:
-    print(f"{num} is not a prime number")
+    print(f"{number} is not a prime number")
 
-choice = input(f"Do you want to find the prime numbers between the range of 0 and {num}? Press Y for 'Yes' N for 'No': ")
+choice = input(f"Do you want to find the prime numbers between the range of 0 and {number}? Press Y for 'Yes' N for 'No': ")
 
 if choice == 'Y':
-    prime_numbers = primes_in_range(0, num)
-    print(f"The prime numbers between 0 and {num} are:"),
+    prime_numbers = primes_in_range(0, number)
+    print(f"The prime numbers between 0 and {number} are:"),
     print(prime_numbers)
 else:
     print("Thank you!")
