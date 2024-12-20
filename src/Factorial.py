a1069db88e3afd8acf9de241e28d6c84af43ef22
Vishalappu21 <vishalanbu582000@gmail.com>
2024-12-20 15:26:44 +0530

    Finding the maximum number in array

--- src/Factorial.py
+++ src/Factorial.py
@@ -1,7 +1,6 @@
-Num1 = int(input("enter the Number1:"))
-factorial = 1
-
+Num1 = int(input("Enter The Number1:"))
+Factorial = 1
 for i in range(1,Num1+1):
-    factorial = factorial * i
+    Factorial = Factorial * i
     
-print(f"The factorial of {Num1} is {factorial}")
+print(f"the factorial number for {Num1} is {Factorial}")
