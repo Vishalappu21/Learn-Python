eebbb570b29ec92b282362bc2726c75bd678c3dd
Vishalappu21 <vishalanbu582000@gmail.com>
2024-12-20 16:46:52 +0530

    Right rotation using slicing logic

--- src/RightArrayRotation.py
+++ src/RightArrayRotation.py
@@ -1,4 +1,4 @@
 arr = list(map(int, input("Enter the elements of the array separated by space: ").split()))
 a = 1
 Right_rotation = arr[-a:] + arr[:-a]
-print(f"the leftRotation of an array is {Right_rotation}")
+print(f"the Right_rotation of an array is {Right_rotation}")
