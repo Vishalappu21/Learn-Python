8763fecdc0d3e1951235bd7e9d1777bf211dfbbf
Vishalappu21 <vishalanbu582000@gmail.com>
2024-12-30 15:16:46 +0530

    Using forLoop logic to find Length

---
+++ src/CountElementInList.py
@@ -0,0 +1,14 @@
+List_count=list(map(int, input("Enter the list to Find the Length:").split()))
+index=0
+for i in List_count:
+    index = index+1
+print(f"The length of the list is :{index}")
+    
+
+
+
+
+
+
+
+
