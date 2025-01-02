113e07e15a51930be8dcc703768504af2d1fb407
Vishalappu21 <vishalanbu582000@gmail.com>
2025-01-02 12:31:35 +0530

    logic to find +ve and -ve number

---
+++ src/Programtoprintpositivenumbers.py
@@ -0,0 +1,8 @@
+L_1 = list(map(int, input("Enter the Element to convert into list:").split()))
+print(L_1)
+for i in L_1:
+    if i > 0:
+        print(i)
+for i in L_1:
+    if i < 0:
+        print(i)
